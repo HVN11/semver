7fa1eaa170f6c88269e81001fc79f50995f205d4
felix baral <felixbaral@me.com>
2021-11-15 13:34:45 +0100

    transform to kotlin multiplatform

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    kotlin("multiplatform") version "1.5.31"
+}
+
+group = "net.swiftzer.semver"
+version = "x"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    ios()
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
+    js(LEGACY) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("org.jetbrains.kotlin:kotlin-test-junit:1.5.31")
+                implementation("junit:junit:4.12")
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+        val iosMain by getting
+        val iosTest by getting
+    }
+}
